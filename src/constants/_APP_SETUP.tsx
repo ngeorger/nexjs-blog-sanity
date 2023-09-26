70875f75908f719875e3d57a9daca688fde5bb31
Nicolas Georger <info@sredevops.cl>
2023-09-26 10:48:03 -0300

    tests mods

--- src/constants/_APP_SETUP.tsx
+++ src/constants/_APP_SETUP.tsx
@@ -1,12 +1,12 @@
-export const WEBSITE_NAME: string = "Syeda Maham Fahim";
-export const WEBSITE_URL: string =
-  "https://syedamaham.dev";
+export const WEBSITE_NAME: string = "SREDEVOPS.ORG - tests";
+export const WEBSITE_URL: string = 
+  "https://sredevops.org";
 
 
-export const LOGO:string="Syeda Maham Fahim"
+export const LOGO:string="loreaa";
 
-export const META_DESCRIPTION= `Join ${WEBSITE_NAME} and explore the latest tech trends with our expert siblings in data engineering, web development, and more. Dive into coding with confidence.`
+export const META_DESCRIPTION= `Join ${WEBSITE_NAME}  Noticias, Tutoriales, Información, Comunidad DevOps, Site Reliability Engineering (SRE) y Platform Engineering 🌎 🇨🇱 🇧🇷 🇪🇸.`
 
-export const META_SEO_KEYWORDS="technology, coding, web development, data engineering, javascript, data science, AWS, cloud, blockchain, insights, Git, GitHub, app development, AWS cloud solutions, data science"
+export const META_SEO_KEYWORDS="technology, coding, web development, data engineering, javascript, data science, AWS, cloud, blockchain, insights, Git, GitHub, app development, AWS cloud solutions, data science";
 
 export const META_CANONICAL=''
